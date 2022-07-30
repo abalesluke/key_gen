92dcaeb68a0c14578445c2cc67cebc271bfcf7fd
Anikin Luke Abales <47096239+ph-fox@users.noreply.github.com>
2022-07-30 15:28:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,27 +10,31 @@ char_pattern = 'abcdef'
 num_pattern = '0123456789'
 
 
-c1 = ''.join(random.choices(char_pattern,k=random.randint(1,4)))
-pad = 8-int(len(c1))
-cc1 = ''.join(random.choices(string.digits,k=pad))
-code1 = ''.join(random.sample(c1+cc1,len(c1+cc1)))
-#print(code1)
-code2 = ''.join(random.choices(string.digits,k=4))
-#print(code2)
-c3 = ''.join(random.choices(char_pattern))
-cc3 = ''.join(random.choices(string.digits,k=3))
-code3 = ''.join(random.sample(c3+cc3,len(c3+cc3)))
-#print(code3)
-c4 = ''.join(random.choices(char_pattern,k=random.randint(1,2)))
-cc4 = ''.join(random.choices(string.digits,k=4-int(len(c4))))
-code4 = ''.join(random.sample(c4+cc4,len(c4+cc4)))
-#print(code4)
-c5 = ''.join(random.choices(char_pattern,k=random.randint(3,4)))
-cc5 = ''.join(random.choices(string.digits,k=12-int(len(c5))))
-code5 = ''.join(random.sample(c5+cc5,len(c5+cc5)))
-#print(code5)
-code = f'{code1}-{code2}-{code3}-{code4}-{code5}'
-print(code)
+for i in range(10):
+	c1 = ''.join(random.choices(char_pattern,k=random.randint(1,4)))
+	pad = 8-int(len(c1))
+	cc1 = ''.join(random.choices(string.digits,k=pad))
+	code1 = ''.join(random.sample(c1+cc1,len(c1+cc1)))
+	#print(code1)
+	code2 = ''.join(random.choices(string.digits,k=4))
+	#print(code2)
+	c3 = ''.join(random.choices(char_pattern))
+	cc3 = ''.join(random.choices(string.digits,k=3))
+	code3 = ''.join(random.sample(c3+cc3,len(c3+cc3)))
+	#print(code3)
+	c4 = ''.join(random.choices(char_pattern,k=random.randint(1,2)))
+	cc4 = ''.join(random.choices(string.digits,k=4-int(len(c4))))
+	code4 = ''.join(random.sample(c4+cc4,len(c4+cc4)))
+	#print(code4)
+	c5 = ''.join(random.choices(char_pattern,k=random.randint(3,4)))
+	cc5 = ''.join(random.choices(string.digits,k=12-int(len(c5))))
+	code5 = ''.join(random.sample(c5+cc5,len(c5+cc5)))
+	#print(code5)
+	code = f'{code1}-{code2}-{code3}-{code4}-{code5}'
+	#print(code)
+	url = 'https://ip-trap.com/bait-page/?trap='+code
+	r = requests.get(url)
+	print(f"[{r.status_code}]::{url}")
 
 """
 r = requests.get(url)
